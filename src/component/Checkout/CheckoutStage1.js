a2d6b1707f7e1cee98b673d4707f8e4fdd19fa63
kongalasrikanth <srikanthkongala9372@gmail.com>
2022-08-16 14:10:41 +0530

    code commit

--- src/component/Checkout/CheckoutStage1.js
+++ src/component/Checkout/CheckoutStage1.js
@@ -1,6 +1,4 @@
 import { React, useEffect } from 'react'
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 
